baa9c1c262376d56024aa79395fc8eaf138b294d
andrea <piermandrea@gmail.com>
2025-05-26 21:11:41 +0200

    Correct error handlers

--- desktop_app/app.c
+++ desktop_app/app.c
@@ -20,13 +20,18 @@ uint8_t check_auth(int serial){
     scanf("%s", password);
     password[strlen(password)] = 0;
     ssize_t sent = write(serial, password, strlen(password)+1);
-    if(sent < 0) assert("Errore scrittura seriale");
-    sleep(1);
+    if(sent < 0) {
+      close(serial);
+      assert("Errore scrittura seriale");
+    }
     
-    ssize_t n = read(serial, risposta, MAX_LENGTH_MSG);
-    if(n<0) assert("Errore nella lettura");
+    ssize_t n = read(serial, risposta, MAX_LENGTH_MSG); // non c'è bisogno della sleep in quanto read bloccante
+    if(n<0) {
+      close(serial);
+      assert("Errore nella lettura");
+    }
     printf("Risposta: [%s]\n", risposta);
-    tcflush(serial, TCIFLUSH);
+    // tcflush(serial, TCIFLUSH); NON ERA QUI L'ERRORE!!!
 
     if(risposta[0] == 'O' && risposta[1] == 'K') return 0;
     cnt++;
@@ -44,18 +49,28 @@ int main() {
   // configuro baud rate
   struct termios options;
   if(tcgetattr(serial, &options) == -1){ // legge impostazioni del terminale seriale
+    close(serial);
     assert("Errore durante acquisizione impostazioni terminale");
   }
 
-  if(cfsetispeed(&options, BAUD) == -1) assert("Errore durante setting input baud rate");
-  if(cfsetospeed(&options, BAUD) == -1) assert("Errore durante setting output baud rate");
+  if(cfsetispeed(&options, BAUD) == -1) {
+    close(serial);
+    assert("Errore durante setting input baud rate");
+  }
+  if(cfsetospeed(&options, BAUD) == -1) {
+    close(serial);
+    assert("Errore durante setting output baud rate");
+  }
   options.c_cflag &= ~PARENB;  // Nessuna parità
   options.c_cflag &= ~CSTOPB;  // 1 bit di stop
   options.c_cflag &= ~CSIZE;   // NO configurazione della dimensione dei dati
   options.c_cflag |= CS8;      // 8 bit di dati
   options.c_lflag &= ~(ICANON | ECHO);  // raw mode - legge subito i dati senza aspettare '\n' e disabilita l'eco: quindi non ricevo dati "sporchi" che mando prima
 
-  if(tcsetattr(serial, TCSANOW, &options) == -1) assert("Errore nell'applicazione degli attributi per seriale"); // 0 per opzioni opzionali
+  if(tcsetattr(serial, TCSANOW, &options) == -1) {
+    close(serial);
+    assert("Errore nell'applicazione degli attributi per seriale"); // 0 per opzioni opzionali
+  }
 
   if(check_auth(serial)){
     printf("Non sei stato correttamente autenticato\nFINE\n");
